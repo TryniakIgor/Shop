71d77dbef5ff1155c0c20136200a77437af50ad0
TryniakIgor <94488222+TryniakIgor@users.noreply.github.com>
2022-07-21 17:37:36 +0300

    fixed refresh token

--- src/main/java/com/example/shop/security/SecurityConfig.java
+++ src/main/java/com/example/shop/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
        http.csrf().disable();
        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-       http.authorizeRequests().antMatchers("api/login/**", "api/token/refresh/**").permitAll();
+       http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh**").permitAll();
        http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("SELLER");
        http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ADMIN");
        http.authorizeRequests().anyRequest().authenticated();
